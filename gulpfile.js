957f14990f68ab188849586c16a21af258f2d656
Cameron Leslie <cameronleslie@gmail.com>
2018-03-13 20:35:05 -0700

    Updating gulp tasks to be more sane.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,129 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+// Developer
+const browserSync = require('browser-sync').create();
+// CSS Imports
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCss = require('gulp-clean-css');
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+////////////////////////////////////////////////////////////////////////////////
+// Settings
+////////////////////////////////////////////////////////////////////////////////
 
-    browserSync.init({
-        server: "./",
-        directory: true
-    });
+// Browser Versions to Transpile/Prefix to.
+const browserVersions = ['ie >= 11', 'last 2 versions'];
 
-    gulp.watch("./src/**/*.scss", ['sass']);
-    // gulp.watch("app/*.html").on('change', browserSync.reload);
+// Build Settings
+const setting = {
+
+  // Default Directories
+  folder: {
+    src: 'src/',
+    dist: 'dist/'
+  },
+
+  // Autoprefixer
+  autoprefixer: {
+    browsers: browserVersions,
+    cascade: true
+  },
+
+  // BrowserSync
+  browserSync: {
+    server: {
+        baseDir: "./dist/",
+        directory: false
+    },
+    ghostMode: {
+      clicks: true,
+      forms: true,
+      scrolls: true
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Builds
+////////////////////////////////////////////////////////////////////////////////
+
+// Html
+gulp.task('html', () => {
+  gulp.src(`${setting.folder.src}**/*.html`)
+    // Write to Dist Folder
+    .pipe(gulp.dest(setting.folder.dist));
 });
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("./src/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("./dist/css/"))
-        .pipe(browserSync.stream());
+// Images
+gulp.task('html', () => {
+  gulp.src(`${setting.folder.src}**/*.{png,gif,jpg,jpeg,svg,bmp}`)
+    // Write to Dist Folder
+    .pipe(gulp.dest(setting.folder.dist));
 });
 
-gulp.task('default', ['serve']);
+// CSS
+gulp.task('css', () => {
+  gulp.src(`${setting.folder.src}**/*.scss`)
+    // Transpile Sass to CSS
+    .pipe(sass().on('error', sass.logError))
+    // Autoprefix
+    .pipe(autoprefixer(setting.autoprefixer))
+    // Minify CSS
+    .pipe(cleanCss())
+    // Writing to Dist folder
+    .pipe(gulp.dest(`${setting.folder.dist}css/`));
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// Development
+////////////////////////////////////////////////////////////////////////////////
+
+// Serve
+gulp.task('serve', () => {
+    browserSync.init(setting.browserSync);
+});
+
+// Reload
+gulp.task('reload', () => {
+  browserSync.reload();
+});
+
+// Watch
+gulp.task('watch', () => {
+  // Watch for updated files in the dist folder and reload
+  gulp.watch(`${setting.folder.dist}**/*.*`, ['reload']);
+  // Watch for changes in source folder and build file
+  gulp.watch(`${setting.folder.src}**/*.scss`, ['css']);
+  gulp.watch(`${setting.folder.src}**/*.html`, ['html']);
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// Meta Tasks
+////////////////////////////////////////////////////////////////////////////////
+
+// Build
+gulp.task('build', ['html', 'css']);
+
+// Develop
+gulp.task('develop', ['build', 'serve', 'watch']);
+
+////////////////////////////////////////////////////////////////////////////////
+// Default
+////////////////////////////////////////////////////////////////////////////////
+
+gulp.task('default', () => {
+  console.log(
+`
+
+Build should be run with 'npm run [command]'
+--------------------------------------------
+Options:
+
+'npm run build'
+    Builds project with no developer features
+
+'npm run develop'
+    Watches and builds
+
+`);
+});
