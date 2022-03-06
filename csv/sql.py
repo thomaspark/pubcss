e25d6e4f8794e47b1cc9cc86f3e3b9980d1c885f
sasaco <sasaicco@gmail.com>
2022-03-06 22:11:00 +0900

    データベース

--- csv/sql.py
+++ csv/sql.py
@@ -16,10 +16,12 @@ def insert_csv():
 
         table_name = table_name[0:-4] # テーブル名から拡張子を削除
 
-        df = pd.read_csv(csv, dtype=object) # CSV 読込
-
+        df = pd.read_csv(csv, header=0) # CSV 読込
+        dft = df.T
+        d = pd.DataFrame(data=dft[1:])
+        d.columns = ['dx','dy','dz','rx','ry','rz']
         with sqlite3.connect(db_path) as conn:
-            df.T.to_sql(table_name, con=conn) # SQLiteにCSVをインポート
+            d.to_sql(table_name, con=conn) # SQLiteにCSVをインポート
 
 if __name__ == '__main__':
     insert_csv()
